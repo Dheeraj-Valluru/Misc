2c799ea8d9d7a7b5da314fee809d98f656d97d24
Dheeraj-Valluru <53436364+Dheeraj-Valluru@users.noreply.github.com>
2021-07-15 10:28:04 -0400

    Add files via upload

---
+++ Classes/Cat.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Cat.h"
+using namespace std;
+
+void Cat::speak() {
+	cout << "meow" << endl;
+}
+
+void Cat::jump() {
+	cout << "Jumping to top of bookcase" << endl;
+}
