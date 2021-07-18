63cd3aa0231464e4cbdcd5dd02d5fa89c622f198
Dheeraj-Valluru <53436364+Dheeraj-Valluru@users.noreply.github.com>
2021-07-18 16:46:58 -0400

    Add files via upload

---
+++ HeadersPrototypes/HeadersPrototypes.cpp
@@ -0,0 +1,16 @@
+#include <iostream> //the angle brackets just mean the library is standard
+
+#include "Utils.h" //tells program to get header file and paste it here (double quotes just mean that the file is in the project)
+
+using namespace std;
+
+int main() {
+
+	hello();
+
+	return 0;
+}
+
+void hello() {
+	cout << "Hello" << endl;
+}
