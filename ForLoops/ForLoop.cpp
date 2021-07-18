86cf637a2176ae32cfd40bdd3bb298fc6f9c636c
Dheeraj-Valluru <53436364+Dheeraj-Valluru@users.noreply.github.com>
2021-07-18 16:45:59 -0400

    Add files via upload

---
+++ ForLoops/ForLoop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	/*
+	for (int i = 0; i < 10; i++) {
+		cout << "Hello" << i << endl;
+	}
+	*/
+
+	for (int i = 0; i < 10; i += 2) {
+		cout << "Count" << i << endl;
+	}
+
+	return 0;
+}
