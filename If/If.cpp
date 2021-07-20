11ca737cb4db60a4e11acb0503690cc40ef49691
Dheeraj-Valluru <53436364+Dheeraj-Valluru@users.noreply.github.com>
2021-07-19 21:11:29 -0400

    Add files via upload

---
+++ If/If.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	string password = "hello";
+
+	cout << "Enter password: " << flush;
+
+	string input;
+	cin >> input;
+
+	if(input == password) {
+		cout << "Access granted" << endl;
+	}
+
+	if (input != password) {
+		cout << "Access denied" << endl;
+	}
+
+	return 0;
+}
