a4b2c004f5e9f51757d352c97f763e947ccd2a43
Dheeraj-Valluru <53436364+Dheeraj-Valluru@users.noreply.github.com>
2022-06-30 10:02:39 -0400

    Week3 of Course 1 if py4e

---
+++ Week3.py
@@ -0,0 +1,22 @@
+#sequential
+x=1
+print(x)
+x+=1
+print(x)
+#exit()
+
+#conditional
+y=10
+if(y<15):
+    print("y is less than 15")
+if(y>20):
+    print("y is greater than 20")
+
+print("program has ended")
+
+#repeated
+n=5
+while n>0:
+    print(str(n) + " seconds left!")
+    n-=1
+print("Blastoff!")
